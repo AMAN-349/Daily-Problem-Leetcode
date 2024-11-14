1bf3bcff50f032209a7142f893639bc26a0781fe
Aman Tyagi <smantyagi349@gmail.com>
2024-11-14 21:57:01 +0530

    Time: 138 ms (18.73%) | Memory: 145.8 MB (10.88%) - LeetSync

--- 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.cpp
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -1,47 +1,44 @@
 class Solution {
 public:
-    bool canDistribute(int x, vector<int>& quantities, int n) {
-        // Pointer to the first not fully distributed product type
-        int j = 0;
-        // Remaining quantity of the jth product type
-        int remaining = quantities[j];
 
-        // Loop through each store
-        for (int i = 0; i < n; i++) {
-            // Check if the remaining quantity of the jth product type
-            // can be fully distributed to the ith store
-            if (remaining <= x) {
-                // If yes, move the pointer to the next product type
+    int find(int x,int n,vector<int> quan)
+    {
+        int j=0;
+        int rem=quan[j];
+        for(int i=0;i<n;i++)
+        {
+            if(rem<=x)
+            {
                 j++;
-                // Check if all products have been distributed
-                if (j == quantities.size()) {
+                if(j==quan.size())
+                {
                     return true;
-                } else {
-                    remaining = quantities[j];
                 }
-            } else {
-                // Distribute the maximum possible quantity (x) to the ith store
-                remaining -= x;
+                else{
+                    rem=quan[j];
+                }
+            }
+            else{
+                rem-=x;
             }
         }
         return false;
     }
-    int minimizedMaximum(int n, vector<int>& quantities) {
-        // Initialize the boundaries of the binary search
-        int left = 0;
-        int right = *max_element(quantities.begin(), quantities.end());
 
-        // Perform binary search until the boundaries converge
-        while (left < right) {
-            int middle = (left + right) / 2;
-            if (canDistribute(middle, quantities, n)) {
-                // Try for a smaller maximum
-                right = middle;
-            } else {
-                // Increase the minimum possible maximum
-                left = middle + 1;
+    int minimizedMaximum(int n, vector<int>& quantities) {
+        int l=0;
+        int r=*max_element(quantities.begin(),quantities.end());
+        while(l<r)
+        {
+            int mid=(l+r)/2;
+            if(find(mid,n,quantities))
+            {
+                r=mid;
+            }
+            else{
+                l=mid+1;
             }
         }
-        return left;
+        return l;
     }
 };
