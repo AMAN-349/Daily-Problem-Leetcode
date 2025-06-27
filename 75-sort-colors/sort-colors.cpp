fce4552c9027aeb8dab78ab32a446e55270f7f6b
Aman Tyagi <smantyagi349@gmail.com>
2025-06-27 17:13:18 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (12.49%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -4,23 +4,24 @@ public:
         int n=nums.size();
         int l=0;
         int r=n-1;
-        int m=0;
-        while(m<=r)
+        vector<int> cnt(4,0);
+        vector<int> aux(n);
+        for(int i=0;i<nums.size();i++)
         {
-            if(nums[m]==1)
-            {
-                m++;
-            }
-            else if(nums[m]==0)
-            {
-                swap(nums[l],nums[m]);
-                l++;
-                m++;
-            }
-            else{
-                swap(nums[r],nums[m]);
-                r--;
-            }
+            cnt[nums[i]]++;
+        }
+        for(int i=1;i<3;i++)
+        {
+            cnt[i]+=cnt[i-1];
+        }
+        for(int i=0;i<n;i++)
+        {
+            aux[cnt[nums[i]]-1]=nums[i];
+            cnt[nums[i]]--;
+        }
+         for(int i=0;i<n;i++)
+        {
+            nums[i]=aux[i];
         }
     }
 };
