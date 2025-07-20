4f0db3184f71cd5aeff3754730c77f5f9b9ad529
Aman Tyagi <smantyagi349@gmail.com>
2025-07-20 21:25:44 +0530

    Time: 4 ms (60.67%) | Memory: 26.4 MB (52.50%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+
+  bool isPal(int i,int j,string s)
+  {
+    while(i<j)
+    {
+        if(s[i]!=s[j])
+        {
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
+  }
+
+    bool validPalindrome(string s) {
+        int n=s.size();
+        int i=0;
+        int j=n-1;
+        int cnt=0;
+        while(i<j)
+        {
+            if(s[i]==s[j])
+            {
+                i++;
+                j--;
+            }
+            else{
+                if(cnt==0)
+                {
+                    return isPal(i,j-1,s) || isPal(i+1,j,s);
+                }
+            }
+        }
+        return i<j?false:true;
+    }
+};
