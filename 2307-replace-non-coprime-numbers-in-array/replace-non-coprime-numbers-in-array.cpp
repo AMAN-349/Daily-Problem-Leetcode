fd10e7bd3abc88c2f5b1db3b87a4f6e45ff1b798
Aman Tyagi <smantyagi349@gmail.com>
2025-09-16 10:19:20 +0530

    Time: 29 ms (31.48%) | Memory: 131.7 MB (22.22%) - LeetSync

--- 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.cpp
+++ 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.cpp
@@ -1,5 +1,15 @@
 class Solution {
 public:
+
+    int gcd(int a,int b)
+    {
+        if(b==0)
+        {
+            return a;
+        }
+        return gcd(b,a%b);
+    }
+
     vector<int> replaceNonCoprimes(vector<int>& nums) {
         vector<int> ans;
         stack<long long> s; 
