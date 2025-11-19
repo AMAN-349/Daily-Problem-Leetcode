5adb64393ad1b45a1ad5f61271b0731a22f05281
Aman Tyagi <smantyagi349@gmail.com>
2025-11-19 11:02:15 +0530

    Time: 0 ms (100.00%) | Memory: 14.5 MB (7.58%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
-        int n = prices.size();
-        // Create a copy of original prices array to store discounted prices
-        vector<int> result = prices;
-
-        for (int i = 0; i < n; i++) {
-            // Look for first smaller or equal price that comes after current
-            // item
-            for (int j = i + 1; j < n; j++) {
-                if (prices[j] <= prices[i]) {
-                    // Apply discount by subtracting prices[j] from current
-                    // price
-                    result[i] = prices[i] - prices[j];
-                    break;
-                }
+        int n=prices.size();
+        stack<int> s;
+        vector<int> ans(n);
+        for(int i=0;i<n;i++)
+        {
+            while(!s.empty() && prices[i]<=prices[s.top()])
+            {
+                ans[s.top()]=prices[s.top()]-prices[i];
+                s.pop();
             }
+            s.push(i);
         }
-
-        return result;
+        while(!s.empty())
+        {
+            ans[s.top()]=prices[s.top()];
+            s.pop();
+        }
+        return ans;
     }
 };
