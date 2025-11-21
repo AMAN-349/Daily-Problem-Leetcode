784cea7f9e06586444978ac25ce1dbd7754d5915
Aman Tyagi <smantyagi349@gmail.com>
2025-11-21 12:02:01 +0530

    Time: 3 ms (78.49%) | Memory: 9.7 MB (68.50%) - LeetSync

--- 686-repeated-string-match/repeated-string-match.cpp
+++ 686-repeated-string-match/repeated-string-match.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     int repeatedStringMatch(string a, string b) {
-        int repeatCount = 1;
-        string s = a;
-
-        while (s.size() < b.size()) {
-            s += a;
-            repeatCount++;
+        int cnt=1;
+        int n=a.size();
+        string temp=a;
+        int m=b.size();
+        if(a.find(b)!=string::npos)
+        {
+            return cnt;
         }
-
-        if (s.find(b) != string::npos) {
-            return repeatCount;
+        while(a.size()<m)
+        {
+            a+=temp;
+            cnt++;
         }
-
-        s += a;
-        repeatCount++;
-
-        if (s.find(b) != string::npos) {
-            return repeatCount;
+        cout<<a<<" ";
+        if(a.find(b)!=string::npos)
+        {
+            return cnt;
         }
-
+        a+=temp;
+        if(a.find(b)!=string::npos)
+        {
+            return cnt+1;
+        }
         return -1;
     }
 };
