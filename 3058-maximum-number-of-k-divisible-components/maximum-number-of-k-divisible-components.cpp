65ff043e1b08d810e01238937061959b0899c22a
Aman Tyagi <smantyagi349@gmail.com>
2025-11-28 21:57:58 +0530

    Time: 165 ms (30.61%) | Memory: 185.7 MB (30.25%) - LeetSync

--- 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.cpp
+++ 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.cpp
@@ -1,49 +1,57 @@
 class Solution {
 public:
-    int maxKDivisibleComponents(int n, vector<vector<int>> &edges,
-                                vector<int> &values, int k) {
-        // Step 1: Create adjacency list from edges
-        vector<int> adjList[n];
-        for (auto edge : edges) {
-            int node1 = edge[0];
-            int node2 = edge[1];
-            adjList[node1].push_back(node2);
-            adjList[node2].push_back(node1);
-        }
-        // Step 2: Initialize component count
-        int componentCount = 0;
-
-        // Step 3: Start DFS traversal from node 0
-        dfs(0, -1, adjList, values, k, componentCount);
-
-        // Step 4: Return the total number of components
-        return componentCount;
-    }
-
-private:
-    int dfs(int currentNode, int parentNode, vector<int> adjList[],
-            vector<int> &nodeValues, int k, int &componentCount) {
-        // Step 1: Initialize sum for the current subtree
-        int sum = 0;
-
-        // Step 2: Traverse all neighbors
-        for (auto neighborNode : adjList[currentNode]) {
-            if (neighborNode != parentNode) {
-                // Recursive call to process the subtree rooted at the neighbor
-                sum += dfs(neighborNode, currentNode, adjList, nodeValues, k,
-                           componentCount);
-                sum %= k;  // Ensure the sum stays within bounds
+    int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
+        if(n==1)
+        {
+            if(values[0]%k==0)
+            {
+                return 1;
             }
         }
+        vector<vector<int>> adjlist(n);
+        vector<int> degree(n, 0);
+        for(auto i:edges)
+        {
+            int val1=i[0];
+            int val2=i[1];
+            adjlist[val1].push_back(val2);
+            adjlist[val2].push_back(val1);
+            degree[val1]++;
+            degree[val2]++;
+        }
+        queue<int> q;
+        for(int i=0;i<n;i++)
+        {
+            if(degree[i]==1)
+            {
+                q.push(i);
+            }
+        }
+        int ans=0;
+        int sum=0;
+        while(!q.empty())
+        {
+            int p=q.front();
+            q.pop();
+            if(values[p]%k==0)
+            {
+                ans++;
+            }
+            for(auto i:adjlist[p])
+            {
+                if(degree[i]>0)
+                {
+                    degree[i]--;
+                    values[i]+=values[p]%k;
+                    if(degree[i]==1)
+                    {
+                        q.push(i);
+                    }
+                }
+            }
+            degree[p]=0;
+        }
+        return ans;
 
-        // Step 3: Add the value of the current node to the sum
-        sum += nodeValues[currentNode];
-
-        // Step 4: Check if the sum is divisible by k
-        sum %= k;
-        if (sum == 0) componentCount++;
-
-        // Step 5: Return the computed sum for the current subtree
-        return sum;
     }
 };
