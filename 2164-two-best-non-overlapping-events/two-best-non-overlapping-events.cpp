147caca0d3b38ea0932a3ba76e08684814ffb6d6
Aman Tyagi <smantyagi349@gmail.com>
2025-12-23 12:02:49 +0530

    Time: 75 ms (68.73%) | Memory: 158.6 MB (85.71%) - LeetSync

--- 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
@@ -1,22 +1,40 @@
 class Solution {
 public:
+    static bool cmp(const vector<int>& a, const vector<int>& b) {
+        return a[0] < b[0];
+    }
+
     int maxTwoEvents(vector<vector<int>>& events) {
-        sort(events.begin(), events.end());
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
-        int maxVal = 0, ans = 0;
-
-        for (const auto& event : events) {
-            int start = event[0], end = event[1], value = event[2];
-
-            while (!pq.empty() && pq.top().first < start) {
-                maxVal = max(maxVal, pq.top().second);
-                pq.pop();
-            }
-
-            ans = max(ans, maxVal + value);
-            pq.push({end, value});
+        int n = events.size();
+        sort(events.begin(), events.end(), cmp);
+        vector<int> suffix(n);
+        suffix[n-1]=events[n-1][2];
+        for(int i=n-2;i>=0;i--)
+        {
+            suffix[i]=max(events[i][2],suffix[i+1]);
         }
 
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int endTime = events[i][1];
+
+            int next = lower_bound(
+                events.begin() + i + 1,
+                events.end(),
+                endTime,
+                [](const vector<int>& e, int value) {
+                    return e[0] <= value;
+                }
+            )-events.begin();
+
+            int temp = 0;
+            if(next<n)
+            {
+                temp=suffix[next];
+            }
+
+            ans = max(ans, events[i][2] + temp);
+        }
         return ans;
     }
 };
