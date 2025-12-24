20fb15a7d8288ca7d8e7e3865c25de1a3ab70ead
Aman Tyagi <smantyagi349@gmail.com>
2025-12-24 13:48:29 +0530

    Time: 2 ms (37.16%) | Memory: 13.4 MB (65.42%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -2,23 +2,21 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int n=nums.size();
-        long long mask=(1L<<31);
         int ans=0;
-        while(mask)
+        for (int bit = 0; bit < 32; bit++)
         {
             int cnt=0;
-            for(int i=0;i<n;i++)
+            for(auto i:nums)
             {
-                if((mask)&(nums[i]))
+                if((i>>bit)&1)
                 {
                     cnt++;
                 }
             }
             if(cnt%3!=0)
             {
-                ans=(ans|mask);
+                ans|=(1<<bit);
             }
-            mask=(mask>>1);
         }
         return ans;
     }
