6d4d6df2041f84419b579443ee3efc7a3d68d096
Aman Tyagi <smantyagi349@gmail.com>
2026-01-08 16:19:45 +0530

    Time: 3 ms (89.75%) | Memory: 23.8 MB (80.81%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -2,10 +2,6 @@ class Solution {
 public:
     static bool cmp(vector<int>& v1,vector<int>& v2)
     {
-        // if(v1[0]==v2[0])
-        // {
-        //     return v1[1]<v2[1];
-        // }
         return v1[0]<v2[0];
     }
 
