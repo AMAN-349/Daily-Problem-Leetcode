6bffa9a6c6cfd47e350cfd73611e253dd3601b36
Aman Tyagi <smantyagi349@gmail.com>
2026-01-11 21:20:32 +0530

    Time: 0 ms (100.00%) | Memory: 71.6 MB (80.08%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,38 +1,14 @@
 class Solution {
 public:
-    //dap means divide and conquer
-
-    int dap(vector<int>& nums,int i,int j)
-    {
-        if(i>j)
-        {
-            return INT_MIN;
-        }
-        int m=(i+j)/2;
-        int suml=dap(nums,i,m-1);
-        int sumr=dap(nums,m+1,j);
-        int summ=nums[m];
-        int k=m-1;
-        int curr=summ;
-        while(k>=i)
-        {
-            curr+=nums[k];
-            summ=max(summ,curr);
-            k--;
-        }
-        k=m+1;
-        curr=summ;
-        while(k<=j)
-        {
-            curr+=nums[k];
-            summ=max(summ,curr);
-            k++;
-        }
-        return max({suml,summ,sumr});
-    }
-
     int maxSubArray(vector<int>& nums) {
         int n=nums.size();
-        return dap(nums,0,n-1);
+        int ans=nums[0];
+        for(int i=1;i<n;i++)
+        {
+            int t=nums[i]+nums[i-1];
+            nums[i]=max(nums[i],t);
+            ans=max(ans,nums[i]);
+        }
+        return ans;
     }
 };
