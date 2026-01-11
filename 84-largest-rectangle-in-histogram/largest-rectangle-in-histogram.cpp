a33d72a45c1bac6c70c0a9410af9cec4a0280199
Aman Tyagi <smantyagi349@gmail.com>
2026-01-11 20:11:33 +0530

    Time: 20 ms (65.61%) | Memory: 79.4 MB (93.26%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -2,50 +2,22 @@ class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
         int n=heights.size();
-        vector<int> prefix(n,0);
-        vector<int> suffix(n,0);
         stack<int> s;
-        for(int i=0;i<n;i++)
-        {
-            while(!s.empty() && heights[s.top()]>heights[i])
-            {
-                prefix[s.top()]=i;
-                s.pop();
-            }
-            s.push(i);
-        }
-        while(!s.empty())
-        {
-            prefix[s.top()]=n;
-            s.pop();
-        }
-        for(int i=n-1;i>=0;i--)
-        {
-            while(!s.empty() && heights[s.top()]>heights[i])
-            {
-                suffix[s.top()]=i;
-                s.pop();
-            }
-            s.push(i);
-        }
-        while(!s.empty())
-        {
-            suffix[s.top()]=-1;
-            s.pop();
-        }
-        for(auto i:prefix)
-        {
-            cout<<i<<" ";
-        }
-        for(auto i:suffix)
-        {
-            cout<<i<<" ";
-        }
         int ans=0;
-        for(int i=0;i<n;i++)
+        for(int i=0;i<=n;i++)
         {
-            int len=prefix[i]-suffix[i]-1;
-            ans=max(ans,len*heights[i]);
+            while(!s.empty() && (i==n || heights[s.top()]>=heights[i]))
+            {
+                int temp=s.top();
+                s.pop();
+                int width;
+                if(s.empty())
+                    width=i;
+                else
+                    width=i-s.top()-1;
+                ans=max(ans,heights[temp]*width);
+            }
+            s.push(i);
         }
         return ans;
     }
