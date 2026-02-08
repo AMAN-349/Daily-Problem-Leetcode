febc461f42ea0edc7760ba1d6b2256fe28487278
Aman Tyagi <smantyagi349@gmail.com>
2026-02-08 19:23:49 +0530

    Time: 0 ms (100.00%) | Memory: 22.8 MB (99.94%) - LeetSync

---
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -0,0 +1,24 @@
+int height(TreeNode* root) {
+    if (root == NULL) {
+        return 0;
+    }
+    return 1 + max(height(root->left), height(root->right));
+}
+
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if (root == NULL) {
+            return true;
+        }
+
+        int leftHeight = height(root->left);
+        int rightHeight = height(root->right);
+
+        if (abs(leftHeight - rightHeight) > 1) {
+            return false;
+        }
+
+        return isBalanced(root->left) && isBalanced(root->right);
+    }
+};
