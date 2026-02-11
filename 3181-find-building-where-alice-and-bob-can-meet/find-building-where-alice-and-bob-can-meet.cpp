75e4b0a92ebc8507310b37a34b6b363a28c9d866
Aman Tyagi <smantyagi349@gmail.com>
2026-02-12 01:34:38 +0530

    Time: 460 ms (48.31%) | Memory: 266.2 MB (64.90%) - LeetSync

--- 3181-find-building-where-alice-and-bob-can-meet/find-building-where-alice-and-bob-can-meet.cpp
+++ 3181-find-building-where-alice-and-bob-can-meet/find-building-where-alice-and-bob-can-meet.cpp
@@ -1,47 +1,109 @@
 class Solution {
 public:
-    vector<int> leftmostBuildingQueries(vector<int>& heights,
-                                        vector<vector<int>>& queries) {
-        vector<pair<int, int>> monoStack;
-        vector<int> result(queries.size(), -1);
-        vector<vector<pair<int, int>>> newQueries(heights.size());
-        for (int i = 0; i < queries.size(); i++) {
-            int a = queries[i][0];
-            int b = queries[i][1];
-            if (a > b) swap(a, b);
-            if (heights[b] > heights[a] || a == b)
-                result[i] = b;
-            else
-                newQueries[b].push_back({heights[a], i});
-        }
+    int n;
+    vector<int> segtree;
 
-        for (int i = heights.size() - 1; i >= 0; i--) {
-            int monoStackSize = monoStack.size();
-            for (auto& [a, b] : newQueries[i]) {
-                int position = search(a, monoStack);
-                if (position < monoStackSize && position >= 0)
-                    result[b] = monoStack[position].second;
-            }
-            while (!monoStack.empty() && monoStack.back().first <= heights[i])
-                monoStack.pop_back();
-            monoStack.push_back({heights[i], i});
+    void build(int i,int l,int r,vector<int>& heights)
+    {
+        if(l==r)
+        {
+            segtree[i]=l;
+            return;
         }
-        return result;
+        int m=(l+r)/2;
+        build(2*i+1,l,m,heights);
+        build(2*i+2,m+1,r,heights);
+        int leftidx=segtree[2*i+1];
+        int rightidx=segtree[2*i+2];
+        if(heights[leftidx]>=heights[rightidx])
+        {
+            segtree[i]=leftidx;
+            return;
+        }
+        segtree[i]=rightidx;
     }
 
-private:
-    int search(int height, vector<pair<int, int>>& monoStack) {
-        int left = 0;
-        int right = monoStack.size() - 1;
-        int ans = -1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (monoStack[mid].first > height) {
-                ans = max(ans, mid);
-                left = mid + 1;
-            } else
-                right = mid - 1;
+    int query(int start,int end,int i,int l,int r,vector<int>& heights)
+    {
+        if(start>r || end<l)
+        {
+            return -1;
         }
+        if(l>=start && r<=end)
+        {
+            return segtree[i];
+        }
+        int m=(l+r)/2;
+        int leftidx=query(start,end,2*i+1,l,m,heights);
+        int rightidx=query(start,end,2*i+2,m+1,r,heights);
+
+        if(leftidx==-1)
+        {
+            return rightidx;
+        }
+        if(rightidx==-1)
+        {
+            return leftidx;
+        }
+        if(heights[leftidx]>=heights[rightidx])
+        {
+            return leftidx;
+        }
+        return rightidx;
+    }
+
+    int binsearch(int a,int b,int start,int end,vector<int>& heights)
+    {
+        int l=start;
+        int r=end;
+        int ans=-1;
+        while(l<=r)
+        {
+            int m=l+(r-l)/2;
+            int temp=query(l,m,0,0,n-1,heights);
+            if(temp!=-1 && heights[temp]>a && heights[temp]>b)
+            {
+                ans=temp;
+                r=m-1;
+            }
+            else{
+                l=m+1;
+            }
+        }
+        return ans;
+
+    }
+
+    vector<int> leftmostBuildingQueries(vector<int>& heights, vector<vector<int>>& queries) {
+        n=heights.size();
+        segtree.resize(4*n,0);
+        build(0,0,n-1,heights);
+        vector<int> ans;
+        for (auto &q : queries) {
+            int i = q[0], j = q[1];
+
+            if (i == j) {
+                ans.push_back(i);
+                continue;
+            }
+
+            int x = min(i, j);
+            int y = max(i, j);
+
+            if (heights[y] > heights[x]) {
+                ans.push_back(y);
+                continue;
+            }
+
+            int temp = query(y + 1, n - 1, 0, 0, n - 1, heights);
+            if (temp == -1 || heights[temp] <= heights[x]) {
+                ans.push_back(-1);
+                continue;
+            }
+
+            ans.push_back(binsearch(heights[x], heights[y], y + 1, n - 1, heights));
+        }
+
         return ans;
     }
 };
