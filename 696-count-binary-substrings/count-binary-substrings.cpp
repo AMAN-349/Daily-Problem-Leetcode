394615b4313475c7b6e7ed4c4fd5ff8d95f581ef
Aman Tyagi <smantyagi349@gmail.com>
2026-02-19 22:04:24 +0530

    Time: 567 ms (5.39%) | Memory: 12.9 MB (82.66%) - LeetSync

---
+++ 696-count-binary-substrings/count-binary-substrings.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int countBinarySubstrings(string s) {
+        int n=s.size();
+        int ans=0;
+        int r=0;
+        char find=s[r]=='0'?'0':'1';
+        int prevcnt=0;
+        while(s[r]==find)
+        {
+            prevcnt++;
+            r++;
+        }
+        cout<<prevcnt<<" ";
+        while(r<n)
+        {
+            int currcnt=0;
+            char next=(find=='0')?'1':'0';
+            while(s[r]==next)
+            {
+                currcnt++;
+                r++;
+            }
+            cout<<currcnt<<" ";
+            ans+=min(prevcnt,currcnt);
+            prevcnt=currcnt;
+            find=next;
+        }
+        return ans;
+    }
+};
