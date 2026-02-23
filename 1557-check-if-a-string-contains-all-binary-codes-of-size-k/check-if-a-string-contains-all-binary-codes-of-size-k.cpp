7284261dda96fa8abebf618fa8a34961c960de45
Aman Tyagi <smantyagi349@gmail.com>
2026-02-24 02:18:28 +0530

    Time: 990 ms (13.47%) | Memory: 122.3 MB (37.70%) - LeetSync

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,29 +1,16 @@
 class Solution {
 public: 
-    int find(string s)
-    {
-        int n=s.size();
-        long long temp=0;
-        for(int i=n-1;i>=0;i--)
-        {
-            if (s[i] == '1')
-            {
-                temp|=(1<<i);
-            }
-        }
-        return temp;
-    }
 
     bool hasAllCodes(string s, int k) {
         int n=s.size();
-        set<int> st;
+        set<string> st;
         string temp=s.substr(0,k);
-        st.insert(find(temp));
+        st.insert(temp);
         for(int i=1;i<n;i++)
         {
             temp.erase(temp.begin());
             temp+=s[i];
-            st.insert(find(temp));
+            st.insert(temp);
         }
         int expected=pow(2,k);
         return st.size()==expected;
