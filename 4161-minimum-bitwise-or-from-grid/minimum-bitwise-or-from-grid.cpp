135e16a5ea4b358efaebfd14928ef8e1c239c78e
Aman Tyagi <smantyagi349@gmail.com>
2026-03-01 23:43:06 +0530

    Time: 35 ms (36.55%) | Memory: 125.9 MB (51.28%) - LeetSync

--- 4161-minimum-bitwise-or-from-grid/minimum-bitwise-or-from-grid.cpp
+++ 4161-minimum-bitwise-or-from-grid/minimum-bitwise-or-from-grid.cpp
@@ -1,33 +1,38 @@
 class Solution {
 public:
     int minimumOR(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m = grid[0].size();
+        int mx = 0;
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                mx = max(mx, grid[i][j]);
+
+        int temp = log2(mx)+1;
+        cout<<temp;
         int ans = 0;
-        int forbidden = 0;
-        
-        for (int bit = 17; bit >= 0; --bit) {
-            int testForbidden = forbidden | (1 << bit);            
-            bool possible = true;
-
-            for (auto &row : grid) {
-                bool found = false;
-
-                for (int num : row) {
-                    if ((num & testForbidden) == 0) {
-                        found = true;
-                        break;
-                    }
+        bool flag=false;
+        int extra=0;
+        for (int i = temp - 1; i >= 0; i--) {
+            int curr=extra|(1<<i);
+            flag=true;
+            for (int j = 0; j < n; j++) {
+                int cnt = 0;
+                for (int k = 0; k < m; k++) {
+                    if ((grid[j][k]) & curr)
+                        cnt++;
                 }
-
-                if (!found) {
-                    possible = false;
+                if (cnt == m) {
+                    ans |= (1LL << i);
+                    cout<<ans<<" ";
+                    flag=false;
                     break;
                 }
             }
-
-            if (possible) {
-                forbidden = testForbidden; 
-            } else {
-                ans |= (1 << bit);   
+            if(flag)
+            {
+                extra=curr;
             }
         }
 
