0fd291c1d2850ed0027495d0d7df710b9a9e41c9
Aman Tyagi <smantyagi349@gmail.com>
2026-03-01 23:35:32 +0530

    Time: 11 ms (56.30%) | Memory: 126 MB (36.04%) - LeetSync

---
+++ 4161-minimum-bitwise-or-from-grid/minimum-bitwise-or-from-grid.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minimumOR(vector<vector<int>>& grid) {
+        int ans = 0;
+        int forbidden = 0;
+        
+        for (int bit = 31; bit >= 0; --bit) {
+            int testForbidden = forbidden | (1 << bit);            
+            bool possible = true;
+
+            for (auto &row : grid) {
+                bool found = false;
+
+                for (int num : row) {
+                    if ((num & testForbidden) == 0) {
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found) {
+                    possible = false;
+                    break;
+                }
+            }
+
+            if (possible) {
+                forbidden = testForbidden; 
+            } else {
+                ans |= (1 << bit);   
+            }
+        }
+
+        return ans;
+    }
+};
