92952df728fca39e95ac06e98e2b6fab1e3f1ce5
Aman Tyagi <smantyagi349@gmail.com>
2026-03-19 16:02:18 +0530

    Time: 27 ms (90.24%) | Memory: 100.8 MB (95.12%) - LeetSync

--- 3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.cpp
+++ 3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.cpp
@@ -3,31 +3,32 @@ public:
     int numberOfSubmatrices(vector<vector<char>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        int res=0;
-        vector<pair<int,int>>dp(m);
-
-        for(auto i:grid)
+        vector<pair<int,int>> dp(m);
+        int ans=0;
+        for(int i=0;i<n;i++)
         {
-            int sum=0, fl=0;
+            int sum=0;
+            int fl=0;
             for(int j=0;j<m;j++)
             {
-                if(i[j]=='X')
+                if(grid[i][j]=='X')
                 {
-                    fl=1;
                     sum++;
+                    fl=1;
                 }
-                else if(i[j]=='Y')
+                else if(grid[i][j]=='Y')
                 {
                     sum--;
                 }
                 dp[j].first|=fl;
                 dp[j].second+=sum;
+
                 if(dp[j].first && dp[j].second==0)
                 {
-                    res++;
+                    ans++;
                 }
             }
         }
-        return res;
+        return ans;
     }
 };
