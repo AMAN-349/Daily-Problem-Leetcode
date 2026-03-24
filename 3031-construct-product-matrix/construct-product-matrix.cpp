4927b606ded26f6e6faf92b2bae749229ee508d7
Aman Tyagi <smantyagi349@gmail.com>
2026-03-24 20:07:08 +0530

    Time: 103 ms (16.97%) | Memory: 165.6 MB (9.59%) - LeetSync

---
+++ 3031-construct-product-matrix/construct-product-matrix.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<vector<int>> constructProductMatrix(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        int mod = 12345;
+
+        vector<vector<int>> prefix(n, vector<int>(m + 1, 1));
+        vector<vector<int>> suffix(n, vector<int>(m + 1, 1));
+
+        int temp = 1;
+
+        for (int i = 0; i < n; i++) {
+            prefix[i][0] = temp;
+            for (int j = 1; j <= m; j++) {
+                prefix[i][j] = (1LL * prefix[i][j - 1] * grid[i][j - 1]) % mod;
+            }
+            temp = prefix[i][m];
+        }
+
+        temp = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            suffix[i][m] = temp;
+            for (int j = m - 1; j >= 0; j--) {
+                suffix[i][j] = (1LL * suffix[i][j + 1] * grid[i][j]) % mod;
+            }
+            temp = suffix[i][0];
+        }
+
+        vector<vector<int>> ans(n,vector<int>(m,1));
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(j>=0)
+                {
+                    ans[i][j]=(1LL*ans[i][j] * prefix[i][j])%mod;
+                }
+                if(j<m)
+                {
+                    ans[i][j]=(1LL*ans[i][j] * suffix[i][j+1])%mod;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
