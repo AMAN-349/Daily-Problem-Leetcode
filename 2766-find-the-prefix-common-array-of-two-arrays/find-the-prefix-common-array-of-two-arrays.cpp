85ece3e55b99702d149c93af5f9182e642bb38d4
Aman Tyagi <smantyagi349@gmail.com>
2026-05-20 07:40:26 +0530

    Time: 5 ms (56.62%) | Memory: 87.2 MB (46.47%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
-        int n = A.size();
-        vector<int> prefixCommonArray(n), frequency(n + 1, 0);
-        int commonCount = 0;
 
-        // Iterate through the elements of both arrays
-        for (int currentIndex = 0; currentIndex < n; ++currentIndex) {
-            // Increment frequency of current elements in A and B
-            // Check if the element in A has appeared before (common in prefix)
-            if (++frequency[A[currentIndex]] == 2) ++commonCount;
-
-            // Check if the element in B has appeared before (common in prefix)
-            if (++frequency[B[currentIndex]] == 2) ++commonCount;
-
-            // Store the count of common elements for the current prefix
-            prefixCommonArray[currentIndex] = commonCount;
+    int find(vector<int>& v1,vector<int>& v2)
+    {
+        int cnt=0;
+        for(int i=0;i<v1.size();i++)
+        {
+            cnt+=min(v1[i],v2[i]);
         }
+        return cnt;
+    }
 
-        // Return the final array with counts of common elements in each prefix
-        return prefixCommonArray;
+    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+        vector<int> ans;
+        vector<int> v1(51,0);
+        vector<int> v2(51,0);
+        int n=A.size();
+        for(int i=0;i<n;i++)
+        {
+            v1[A[i]]++;
+            v2[B[i]]++;
+            ans.push_back(find(v1,v2));
+        }
+        return ans;
     }
 };
