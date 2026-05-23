c8ebe55e2966f5391644704838c4304d82d3c63e
Aman Tyagi <smantyagi349@gmail.com>
2026-05-23 23:45:58 +0530

    Time: 16 ms (33.11%) | Memory: 38.6 MB (26.40%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,33 +1,21 @@
 class Solution {
 public:
-
-    long long countSubarraysLessThanTarget(vector<int>& arr, int target) {
-        int n = arr.size();
-
-        int left = 0;
-        long long sum = 0;
-        long long count = 0;
-
-        for (int right = 0; right < n; right++) {
-
-            sum += arr[right];
-
-            // Shrink window until sum < target
-            while (left <= right && sum > target) {
-                sum -= arr[left];
-                left++;
-            }
-
-            // All subarrays ending at right
-            // and starting from left to right are valid
-            count += (right - left + 1);
-        }
-
-        return count;
-    }
-
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        return countSubarraysLessThanTarget(nums,goal)-countSubarraysLessThanTarget(nums,goal-1);
+        int n=nums.size();
+        unordered_map<int,int> m;
+        int sum=0;
+        int cnt=0;
 
+        for(int i=0;i<n;i++)
+        {
+            sum+=nums[i];
+            if(sum==goal) cnt++;
+            if(m.find(sum-goal)!=m.end())
+            {
+                cnt+=m[sum-goal];
+            }
+            m[sum]++;
+        }
+        return cnt;
     }
 };
