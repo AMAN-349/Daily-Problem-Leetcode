cd307ccc526a367faa14c5280deb9784ce681460
Aman Tyagi <smantyagi349@gmail.com>
2026-05-24 16:43:05 +0530

    Time: 7 ms (94.19%) | Memory: 19 MB (76.45%) - LeetSync

---
+++ 1466-jump-game-v/jump-game-v.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int n;
+    vector<int> dp;
+
+    int maxseg(vector<int>& arr,int start,int end)
+    {
+        if(start>end) return 0;
+        int maxi=0;
+        for(int i=start;i<=end;i++)
+        {
+            maxi=max(maxi,arr[i]);
+        }
+        return maxi;
+    }
+
+    int find(int ind,vector<int>& arr,int d)
+    {
+        if(dp[ind]!=-1) return dp[ind];
+        int ans=1;
+        for(int i=ind+1;i<=min(n-1,ind+d);i++)
+        {
+            if(arr[i] >= arr[ind])
+                break;
+
+            ans = max(ans, 1 + find(i, arr, d));
+        }
+
+        for(int i=ind-1;i>=max(0,ind-d);i--)
+        {
+            if(arr[i] >= arr[ind])
+                break;
+
+            ans = max(ans, 1 + find(i, arr, d));
+        }
+        return dp[ind]=ans;
+    }
+
+    int maxJumps(vector<int>& arr, int d) {
+        n=arr.size();
+        dp.resize(n+1,-1);
+        int ans=1;
+
+        for(int i=0;i<n;i++)
+        {
+            int ansi=find(i,arr,d);
+            ans=max(ans,ansi);
+        }
+
+        return ans;
+    }
+};
