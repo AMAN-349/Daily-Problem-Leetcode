c6c1fe83b6d88a186c36a9ebb5dfc7aadd786d1d
Aman Tyagi <smantyagi349@gmail.com>
2026-06-26 23:57:29 +0530

    Time: 571 ms (23.18%) | Memory: 187.4 MB (26.49%) - LeetSync

--- 4075-count-subarrays-with-majority-element-ii/count-subarrays-with-majority-element-ii.cpp
+++ 4075-count-subarrays-with-majority-element-ii/count-subarrays-with-majority-element-ii.cpp
@@ -1,62 +1,37 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#define ll long long int
+using namespace __gnu_pbds;
+template<class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
 class Solution {
 public:
-
-    vector<int> bit;
-    int N;
-    int sum(int i)
-    {
-        int ans=0;
-        for(;i>0;i-=(i&-i))
-        {
-            ans+=bit[i];
-        }
-        return ans;
-    }
-
-    void update(int i,int x)
-    {
-        for(;i<N;i+=(i&-i))
-        {
-            bit[i]+=x;
-        }
-    }
-
     long long countMajoritySubarrays(vector<int>& nums, int target) {
+        long long ans=0;
         int n=nums.size();
-        N=n+5;
-        bit.resize(N,0);
+
+        vector<int> prefsum(n,0);
         for(int i=0;i<n;i++)
         {
             if(nums[i]==target)
             {
-                nums[i]=1;
+                prefsum[i]=1;
             }
             else{
-                nums[i]=-1;
+                prefsum[i]=-1;
             }
         }
+
         for(int i=1;i<n;i++)
         {
-            nums[i]+=nums[i-1];
+            prefsum[i]+=prefsum[i-1];
         }
-        long long ans=0;
-        map<int,int> m;
-        for(auto i:nums)
-        {
-            m[i];
-        }
-        m[0];
-        int cnt=1;
-        for(auto& i:m)
-        {
-            i.second=cnt++;
-        }
-        update(m[0],1);
+
+        ordered_set<pair<int,int>> st;
+        st.insert({0,0});
         for(int i=0;i<n;i++)
         {
-            int pos=m[nums[i]];
-            ans+=sum(pos-1);
-            update(pos,1);
+            ans+=st.order_of_key({prefsum[i],-1});
+            st.insert({prefsum[i],i});
         }
         return ans;
     }
